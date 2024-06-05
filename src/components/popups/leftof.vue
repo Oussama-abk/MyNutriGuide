88686352846462a7a7a542f2e1da2f04020f70f7
FGFCONSULTANT <abakarimoussama@gmail.com>
2024-06-05 10:45:43 +0200

    popup edit

--- src/components/popups/leftof.vue
+++ src/components/popups/leftof.vue
@@ -3,7 +3,7 @@
       <div v-show="show" class="background-overlay"></div>
       <div v-show="show" class="popup-container">
         <div class="popup-header">
-          <p class="popup-title">Veuillez saisir le plat que vous souhaitez :</p>
+          <p class="popup-title">Veuillez saisir l’ingrédient que vous souhaitez</p>
           <svg class="close-button" @click="closePopup" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"/></svg>
         </div>
         <div class="popup-content">
